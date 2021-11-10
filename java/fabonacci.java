92de7457bb623e1aec6252cf76cef6c942fb80ff
Your Name <you@example.com>
2021-11-10 22:23:43 +0530

    okie

--- java/fabonacci.java
+++ java/fabonacci.java
@@ -1,16 +1,18 @@
-class fabonacci example1{
+class fabonacci{
 
     public static void main(String args[]) {
         {
             int n1=0,n2=1,n3,i,count=10;
-            system.out.print(n1+" "+n2);
+            System.out.print(n1+" "+n2);
 
             for(i=2; i<count; i++)
             {
-                n3=n1+n2
-                system.out.print(""+n3)
+                n3=n1+n2;
+                System.out.print(""+n3);
                 n1=n2;
                 n2=n3;
 
             }
-        }}
+        }
+}
+
